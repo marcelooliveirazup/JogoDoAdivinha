42e4605c9f7d264735460b49b095c98b041fc262
marcelooliveirazup <marcelo.oliveira@zup.com.br>
2024-12-16 14:19:23 -0300

    Prompt para o jogador digitar seu palpite e condicao caso ele não queira mais jogar.

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,49 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import javax.swing.JOptionPane;
+import java.util.Random;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+
+    public static void main(String[] args) {
+        Random random = new Random();
+        int pontuacaoTotal = 0;
+        StringBuilder numerosAcertados = new StringBuilder();
+        StringBuilder numerosErrados = new StringBuilder();
+
+        // Menu de nível de dificuldade
+        String[] niveis = {"Fácil (1 a 10)", "Médio (1 a 50)", "Difícil (1 a 100)"};
+        int escolha = JOptionPane.showOptionDialog(
+                null,
+                "Escolha o nível de dificuldade:",
+                "Jogo do Adivinha",
+                JOptionPane.DEFAULT_OPTION,
+                JOptionPane.INFORMATION_MESSAGE,
+                null,
+                niveis,
+                niveis[0]
+        );
+
+        int intervalo = switch (escolha) {
+            case 0 -> 10;
+            case 1 -> 50;
+            case 2 -> 100;
+            default -> 10;
+        };
+
+        boolean continuar = true;
+        while (continuar) {
+            int numeroSorteado = random.nextInt(intervalo) + 1;
+
+            // Pede ao jogador para digitar seu palpite
+            String entrada = JOptionPane.showInputDialog(
+                    null,
+                    "Digite seu palpite (1 a " + intervalo + "):",
+                    "Jogo do Adivinha",
+                    JOptionPane.QUESTION_MESSAGE
+            );
+
+            // Encerra se o jogador cancelar
+            if (entrada == null) break;
         }
-    }
+
+
 }
